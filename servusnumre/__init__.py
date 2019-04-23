bd2e0b10aefdf6fb201155fddc3e23afaa236a45
Carlos A. Planchón <bubbledoloresuruguay2@gmail.com>
2019-04-23 20:44:18 -0300

    Update __init__.py

--- servusnumre/__init__.py
+++ servusnumre/__init__.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 
-float_pattern = r"[-+]?[0-9]*[\.,]?[0-9]+"
-exponent_pattern = r"[eE]?[-+]?[0-9]?"
+float_pattern: str = r"[-+]?[0-9]*[\.,]?[0-9]+"
+exponent_pattern: str = r"[eE]?[-+]?[0-9]?"
